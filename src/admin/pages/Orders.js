6ae3b6f9010d6ba28cac28b101c9fd2c3b4b255e
thanhvp <phuocthanh20cgh@gmail.com>
2023-03-12 17:40:10 +0700

    update order

--- src/admin/pages/Orders.js
+++ src/admin/pages/Orders.js
@@ -3,14 +3,18 @@ import { useEffect, useState } from "react"
 
 function Orders() {
   const getOrders = () => {
-    return fetch("https://dummyjson.com/carts/1").then((res) => res.json());
+    return fetch("https://server-buildingpc.herokuapp.com/cart/getcart?userID=PhuongThai").then((res) => res.json());
   };
+
+  var x = 1000;
+x = x.toLocaleString('en-US', {style : 'currency', currency : 'VND'});
+console.log(x);
     const[loading,setLoading] = useState(false);
     const[dataSource,setDataSource] = useState([])
     useEffect(()=>{
         setLoading(true)
         getOrders().then(res=>{
-            setDataSource(res.products)
+            setDataSource(res.Product)
             setLoading(false)
         })
     },[])
@@ -20,32 +24,33 @@ function Orders() {
             <Table
             loading={loading}
              columns={[
+           
                 {
-
-                title:"Title",
-                dataIndex:"title",
+                title:"Name",
+                dataIndex:"name",
             },
             {
 
                 title:"Price",
                 dataIndex:"price",
-                render:(value)=><span>${value}</span>
+                render:(value)=><span>{value}đ</span>
             },
+      
             {
-              title: "DiscountedPrice",
-              dataIndex: "discountedPrice",
-              render: (value) => <span>${value}</span>,
-            },
-            {
-              title: "Quantity",
-              dataIndex: "quantity",
+              title: "Amount",
+              dataIndex: "amount",
             },
             {
               title: "Total",
-              dataIndex: "total",
-            }
-
-
+              dataIndex: "totally",
+            },
+            {
+              title:"Photo",
+              dataIndex:"image",
+              render:(link) =>{
+                  return <Avatar src={link}/>
+              }
+          },
 
             ]}
             dataSource={dataSource}
